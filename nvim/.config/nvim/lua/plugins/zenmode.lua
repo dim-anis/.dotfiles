168e72e33cb73c848c89634e76b1a89831d69d34
dim-anis <anis.dim@gmail.com>
2024-02-12 16:54:22 +0700

    update zenmod config

--- nvim/.config/nvim/lua/plugins/zenmode.lua
+++ nvim/.config/nvim/lua/plugins/zenmode.lua
@@ -1,20 +1,18 @@
--- { "<leader>/", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Search Current Buffer" },
 return {
 	"folke/zen-mode.nvim",
-	-- keys = {
-	-- 	{
-	-- 		"<leader>zz",
-	-- 		function()
-	-- 			require("zen-mode").toggle()
-	-- 		end,
-	-- 		desc = "ZenMode",
-	-- 	},
-	-- 	{
-	-- 		"<leader>zr",
-	-- 		function()
-	-- 			require("zen-mode").toggle()
-	-- 		end,
-	-- 		desc = "ZenMode",
-	-- 	},
-	-- },
+	opts = {
+		window = {
+			backdrop = 1,
+			width = 120,
+			height = 1,
+			options = {
+				signcolumn = "no",
+				number = false,
+				relativenumber = false,
+			},
+		},
+		plugins = {
+			tmux = { enabled = false },
+		},
+	},
 }
