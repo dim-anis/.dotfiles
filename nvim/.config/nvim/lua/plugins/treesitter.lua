2c3f2d2000fa71ce10a09a1f08a216f920ebf141
dim-anis <anis.dim@gmail.com>
2024-02-22 16:00:59 +0700

    textobjects remap

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,54 +1,53 @@
 return {
-	{
+	{ -- Highlight, edit, and navigate code
 		"nvim-treesitter/nvim-treesitter",
-		version = "*",
-		build = ":TSUpdate",
-		event = { "BufReadPost", "BufNewFile" },
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter-textobjects",
 		},
-		cmd = { "TSUpdateSync" },
+		build = ":TSUpdate",
 		config = function()
+			-- [[ Configure Treesitter ]]
+			-- See `:help nvim-treesitter`
+			-- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
+			---@diagnostic disable-next-line: missing-fields
 			require("nvim-treesitter.configs").setup({
+				-- Add languages to be installed here that you want installed for treesitter
 				ensure_installed = {
-					"bash",
+					"c",
 					"go",
-					"html",
-					"javascript",
-					"json",
 					"lua",
+					"python",
+					"rust",
+					"tsx",
 					"markdown",
 					"markdown_inline",
-					"python",
-					"query",
-					"regex",
-					"tsx",
+					"javascript",
 					"typescript",
-					"vim",
 					"vimdoc",
-					"yaml",
-					"rust",
+					"vim",
+					"bash",
 				},
+
+				-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+				auto_install = false,
+				-- Install languages synchronously (only applied to `ensure_installed`)
 				sync_install = false,
-				auto_install = true,
-				highlight = {
-					enable = true,
-					additional_vim_regex_highlighting = { "markdown_inline" },
-				},
+				-- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
+				highlight = { enable = true },
 				indent = { enable = true },
 				incremental_selection = {
 					enable = true,
 					keymaps = {
-						init_selection = "<C-space>",
-						node_incremental = "<C-space>",
-						scope_incremental = false,
-						node_decremental = "<bs>",
+						init_selection = "<c-space>",
+						node_incremental = "<c-space>",
+						scope_incremental = "<c-s>",
+						node_decremental = "<M-space>",
 					},
 				},
 				textobjects = {
 					select = {
 						enable = true,
-						lookahead = true,
+						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
 						keymaps = {
 							-- You can use the capture groups defined in textobjects.scm
 							["af"] = { query = "@function.outer", desc = "around a function" },
@@ -75,24 +74,30 @@ return {
 					move = {
 						enable = true,
 						set_jumps = true, -- whether to set jumps in the jumplist
-						goto_previous_start = {
-							["[f"] = { query = "@function.outer", desc = "Previous function" },
-							["[c"] = { query = "@class.outer", desc = "Previous class" },
-							["[p"] = { query = "@parameter.inner", desc = "Previous parameter" },
-						},
 						goto_next_start = {
-							["]f"] = { query = "@function.outer", desc = "Next function" },
-							["]c"] = { query = "@class.outer", desc = "Next class" },
-							["]p"] = { query = "@parameter.inner", desc = "Next parameter" },
+							["]m"] = "@function.outer",
+							["]]"] = "@class.outer",
 						},
-						swap = {
-							enable = true,
-							swap_next = {
-								["<leader>a"] = "@parameter.inner",
-							},
-							swap_previous = {
-								["<leader>A"] = "@parameter.inner",
-							},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
 						},
 					},
 				},
