1a1fea59acbd1d5d8e9ceca5c8d46aad614c2b49
dim-anis <anis.dim@gmail.com>
2024-04-08 16:24:50 +0700

    enable eslint flatconfig in eslint lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -77,6 +77,9 @@ return {
 					settings = {
 						-- helps eslint find the eslintrc when it's placed in a subfolder instead of the cwd root
 						workingDirectory = { mode = "auto" },
+						experimental = {
+							useFlatConfig = true,
+						},
 					},
 				},
 				jsonls = {
