3e83552b197de5f1cc44b5e922d1acc14f077844
dim-anis <anis.dim@gmail.com>
2024-04-08 16:37:58 +0700

    minor refactoring

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -3,8 +3,15 @@ return {
 	event = "VeryLazy",
 	lazy = true,
 	config = function()
-		vim.o.timeout = true
-		vim.o.timeoutlen = 300
 		require("which-key").setup()
+
+		-- Document existing key chains
+		require("which-key").register({
+			["<leader>c"] = { name = "[C]ode", _ = "which_key_ignore" },
+			["<leader>d"] = { name = "[D]ocument", _ = "which_key_ignore" },
+			["<leader>r"] = { name = "[R]ename", _ = "which_key_ignore" },
+			["<leader>s"] = { name = "[S]earch", _ = "which_key_ignore" },
+			["<leader>w"] = { name = "[W]orkspace", _ = "which_key_ignore" },
+		})
 	end,
 }
