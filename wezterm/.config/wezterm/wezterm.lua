7672cae362ed5507d21b2f659305bfe9aa8aafe2
dim-anis <anis.dim@gmail.com>
2025-02-19 13:04:23 +0700

    remove comments

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,15 +4,13 @@ local wezterm = require("wezterm")
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
+-- Colors & Appearance
 config.color_scheme = "catppuccin-mocha"
-config.font = wezterm.font("Hack Nerd Font")
-config.font_size = 16.0
-
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
 
--- and finally, return the configuration to wezterm
+-- Fonts
+config.font = wezterm.font("Hack Nerd Font")
+config.font_size = 16.0
+
 return config
